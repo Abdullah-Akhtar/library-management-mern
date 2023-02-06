f8bfc6270cf6f6c823893544fa7caf43bc689be0
Abdullah-Akhtar <mak.abdullah07@gmail.com>
2023-02-06 16:15:03 +0500

    asd

---
+++ src/routes/SignUp.js
@@ -0,0 +1,12 @@
+import { Form } from "react-router-dom";
+
+export default function Contact() {
+  
+
+  return (
+    <>
+    <h1>SignUp</h1>
+    <a href={`/signIn`}>Your Name</a>
+    </>
+  )
+}
---
+++ src/routes/signIn.js
@@ -0,0 +1,8 @@
+export default function signIn() {
+    return (
+      <>
+        <h1>Sign In</h1>
+        <a href={`/signUp/`}>Your Name</a>
+      </>
+    );
+  }
